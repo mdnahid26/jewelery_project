f6d365c3a2bc134e81d7017d263d2d8e24cb2870
mdnahid26 <naimulnihal2696@gmail.com>
2021-08-31 17:25:19 +0600

    1st upload

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.mercury_slick').slick({
+        prevArrow:'<i class="fas fa-chevron-left mercury_arrow left"></i>',
+        nextArrow:'<i class="fas fa-chevron-right mercury_arrow right"></i>'
+    })
+})
